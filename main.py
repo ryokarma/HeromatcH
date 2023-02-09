2566a3794e8633bc06c157d49efbfffbfb3d9ca4
ryokarma <alexandreferrermk@gmail.com>
2023-02-09 16:11:23 +0100

    Les deux héros sont forcément différents car hero2 tiré sur une liste dont hero1 a été supprimé. Comme ça pas de boucle qui tourne dans le vide si hero1 == hero2. Reduction du nombre de noms pour les tests.

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-with open("names.txt", "r") as file:
-    name_list = file.read().split()
+test = True
+while test:
 
-import random
+    with open("names.txt", "r") as file:
+        name_list = file.read().split()
 
-random_name_1 = random.choice(name_list)
-random_name_2 = random.choice(name_list)
+    import random
 
-left_hero = random_name_1
-left_wins = 0
-left_loses = 0
+    random_name_1 = random.choice(name_list)
+    name_list.remove(random_name_1)
+    random_name_2 = random.choice(name_list)
 
-right_hero = random_name_2
-right_wins = 0
-right_loses = 0
 
-user_okay = True
 
-if left_hero == right_hero:
-    exit()
+    left_hero = random_name_1
+    left_wins = 0
+    left_loses = 0
 
-else:
-    while user_okay == True:
+    right_hero = random_name_2
+    right_wins = 0
+    right_loses = 0
+
+    user_okay = True
+    while user_okay:
         user_choice = input(f"Tapez 1 si vous votez pour {left_hero}, ou 2 si vous votez pour {right_hero}.")
         if user_choice == "1":
             left_wins += 1
@@ -35,4 +36,6 @@ else:
         else:
             print("Merci de taper uniquement 1 ou 2, espèce de gros naze.")
             user_okay = True
-        
+
+
+
