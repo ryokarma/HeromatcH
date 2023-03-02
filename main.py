b39200d6083f42032f912cf9019e0bb658daf327
ryokarma <alexandreferrermk@gmail.com>
2023-03-02 13:46:21 +0100

    mise en place de la base de donnée json

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import random
+import json
+
+with open("names.txt", "r") as file:
+    name_list = [line.strip() for line in file.readlines()]
+
+name_dict = {name: 0 for name in name_list}
+
+names_json = json.dumps(name_dict)
+with open("names_json.json", "w") as f:
+    f.write(names_json)
 
 for i in range(3):
-
-    with open("names.txt", "r") as file:
-        name_list = [line.strip() for line in file.readlines()]
 
     left_hero = random.choice(name_list)
     name_list.remove(left_hero)
     right_hero = random.choice(name_list)
 
     user_okay = True
-    with open("choices.txt", "a") as choices_file:
-        while user_okay:
-            user_choice = input(f"Tapez 1 si vous votez pour {left_hero}, ou 2 si vous votez pour {right_hero}.")
-            if user_choice == "1":
-                print(f"Vous avez voté pour {left_hero} !")
-                choices_file.write(left_hero + "\n")
-                user_okay = False
-            elif user_choice == "2":
-                print(f"Vous avez voté pour {right_hero} !")
-                choices_file.write(right_hero + "\n")
-                user_okay = False
-            else:
-                print("Merci de taper uniquement 1 ou 2, espèce de gros naze.")
-                user_okay = True
-
-score = {}
-
-with open("choices.txt", "r") as choices_file:
-    for line in choices_file:
-        name = line.strip()
-        if name in score:
-            score[name] += 1
+    while user_okay:
+        user_choice = input(f"Tapez 1 si vous votez pour {left_hero}, ou 2 si vous votez pour {right_hero}. ")
+        if user_choice == "1":
+            print(f"Vous avez voté pour {left_hero} !")
+            with open("names_json.json", "r+") as names_json:
+                data = json.load(names_json)
+                data[left_hero] += 1
+                names_json.seek(0)
+                names_json.truncate(0)
+                json.dump(data, names_json)
+            user_okay = False
+        elif user_choice == "2":
+            print(f"Vous avez voté pour {right_hero} !")
+            with open("names_json.json", "r+") as names_json:
+                data = json.load(names_json)
+                data[right_hero] += 1
+                names_json.seek(0)
+                names_json.truncate(0)
+                json.dump(data, names_json)
+            user_okay = False
         else:
-            score[name] = 1
+            print("Merci de taper uniquement 1 ou 2.")
+            user_okay = True
 
-print(score)
+with open("names_json.json", "r") as names_json:
+    data = json.load(names_json)
+    print(data)
